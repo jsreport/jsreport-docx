19b9a2a13a6541a1d2794a7623220984446ad2e7
BJR Matos <bjrmatos@gmail.com>
2019-09-17 13:25:04 -0500

    add beta warning dialog

--- jsreport.config.js
+++ jsreport.config.js
@@ -3,6 +3,16 @@ const office = require('jsreport-office')
 module.exports = {
   'name': 'docx',
   'main': 'lib/docx.js',
-  'optionsSchema': office.extendSchema('docx', {}),
+  'optionsSchema': office.extendSchema('docx', {
+    type: 'object',
+    properties: {
+      beta: {
+        type: 'object',
+        properties: {
+          showWarning: { type: 'boolean', default: true }
+        }
+      }
+    }
+  }),
   'dependencies': []
 }
--- studio/main_dev.js
+++ studio/main_dev.js
@@ -35,6 +35,48 @@ Studio.addApiSpec({
   }
 })
 
+const pendingModalsLaunch = []
+
+const pendingModalsInterval = setInterval(() => {
+  if (pendingModalsLaunch.length === 0 || Studio.isModalOpen()) {
+    return
+  }
+
+  const toLaunch = pendingModalsLaunch.splice(0, 1)
+
+  toLaunch[0]()
+
+  if (pendingModalsLaunch.length === 0) {
+    clearInterval(pendingModalsInterval)
+  }
+}, 200)
+
+Studio.previewListeners.push((request, entities) => {
+  if (request.template.recipe !== 'docx') {
+    return
+  }
+
+  if (Studio.extensions.docx.options.beta.showWarning === false) {
+    return
+  }
+
+  if (Studio.getSettingValueByKey('beta-docx-informed', false) === true) {
+    return
+  }
+
+  Studio.setSetting('beta-docx-informed', true)
+
+  const launchBetaModal = () => {
+    Studio.openModal(() => (
+      <div>
+        docx recipe is currently in the beta phase and in continuous development. There're use cases it doesn't support yet but we get there soon if you help us with <a href='https://forum.jsreport.net' target='_blank'>feedback</a>. Please note there can be breaking changes in the next versions of the recipe until we reach stable API.
+      </div>
+    ))
+  }
+
+  pendingModalsLaunch.push(launchBetaModal)
+})
+
 Studio.previewListeners.push((request, entities) => {
   if (request.template.recipe !== 'docx') {
     return
@@ -54,9 +96,19 @@ Studio.previewListeners.push((request, entities) => {
 
   Studio.setSetting('office-preview-informed', true)
 
-  Studio.openModal(() => <div>
-    We need to upload your docx report to our publicly hosted server to be able to use
-    Office Online Service for previewing here in the studio. You can disable it in the configuration, see <a
-      href='https://jsreport.net/learn/docx' target='_blank'>https://jsreport.net/learn/docx</a> for details.
-  </div>)
+  const launchOfficeModal = () => {
+    Studio.openModal(() => <div>
+      We need to upload your docx report to our publicly hosted server to be able to use
+      Office Online Service for previewing here in the studio. You can disable it in the configuration, see <a
+        href='https://jsreport.net/learn/docx' target='_blank'>https://jsreport.net/learn/docx</a> for details.
+    </div>)
+  }
+
+  pendingModalsLaunch.push(launchOfficeModal)
+})
+
+Studio.previewListeners.push(() => {
+  if (pendingModalsLaunch.length === 0) {
+    clearInterval(pendingModalsInterval)
+  }
 })
