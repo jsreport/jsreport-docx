96c515b4db21ec94de2229558b2ae6d9a7125979
BJR Matos <bjrmatos@gmail.com>
2020-08-18 20:59:39 -0500

    add bubbleChart support

--- test/test.js
+++ test/test.js
@@ -2112,6 +2112,63 @@ describe('docx', () => {
     })
   })
 
+  it('bubble chart', async () => {
+    const labels = [
+      2.3,
+      1.4,
+      4.2,
+      3.1,
+      2.5
+    ]
+
+    const datasets = [{
+      label: 'Y Values',
+      data: [
+        [4.6, 6],
+        [3.2, 8],
+        [5.4, 3],
+        [2.1, 7],
+        [1.5, 2]
+      ]
+    }]
+
+    const result = await reporter.render({
+      template: {
+        engine: 'handlebars',
+        recipe: 'docx',
+        docx: {
+          templateAsset: {
+            content: fs.readFileSync(path.join(__dirname, 'bubble-chart.docx'))
+          }
+        }
+      },
+      data: {
+        chartData: {
+          labels,
+          datasets
+        }
+      }
+    })
+
+    const files = await decompress()(result.content)
+
+    const doc = new DOMParser().parseFromString(
+      files.find(f => f.path === 'word/charts/chart1.xml').data.toString()
+    )
+
+    const dataElements = nodeListToArray(doc.getElementsByTagName('c:ser'))
+
+    dataElements.forEach((dataEl, idx) => {
+      dataEl.getElementsByTagName('c:tx')[0].getElementsByTagName('c:v')[0].textContent.should.be.eql(datasets[idx].label)
+
+      nodeListToArray(dataEl.getElementsByTagName('c:xVal')[0].getElementsByTagName('c:v')).map((el) => el.textContent).should.be.eql(labels.map((l) => l.toString()))
+
+      nodeListToArray(dataEl.getElementsByTagName('c:yVal')[0].getElementsByTagName('c:v')).map((el) => parseFloat(el.textContent)).should.be.eql(datasets[idx].data.map((d) => d[0]))
+
+      nodeListToArray(dataEl.getElementsByTagName('c:bubbleSize')[0].getElementsByTagName('c:v')).map((el) => parseFloat(el.textContent)).should.be.eql(datasets[idx].data.map((d) => d[1]))
+    })
+  })
+
   it('stock chart', async () => {
     const labels = [
       '2020-05-10',
